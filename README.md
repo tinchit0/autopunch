8b9bb541f54901732adcbc35588bd632a8e40512
Martín H. Campos <camposm@aia.es>
2024-09-13 01:04:40 +0200

    feat: now there's only one punch endpoint, and you punch in or out depending if you are currently working or not. also there's new 'program' endpoint that programs cloud run executions based on the expected number of working hours

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 Así verás que no se me olvida fichar
 
-Fichar:
+Fichar/desfichar. Entra en timenet, si dice que no estoy trabajando ficha, y si sí estoy trabajando, desficha.
 ```
-docker run autopunch in
+docker run autopunch punch
 ```
 
-Desfichar:
+Programar fichajes. (GCP auth needed) Entra en timenet, mira el número de horas que se espera que trabaje en el día actual, y planifica un Cloud Run de GCP con el comando de antes para las 9hs y para las 9+{numero de horas esperadas}.
 ```
-docker run autopunch out
+docker run --rm -v ./keys:/keys -e GOOGLE_APPLICATION_CREDENTIALS=/keys/key.json autopunch program
 ```
--- autopunch.py
+++ autopunch.py
@@ -4,6 +4,8 @@ from contextlib import contextmanager
 from datetime import date
 
 import click
+from google.cloud import scheduler_v1
+from google.protobuf import field_mask_pb2
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
@@ -44,6 +46,13 @@ def enter_timenet(headless=True):
             driver.quit()
 
 
+def am_i_working(driver):
+    state_label = driver.find_element(
+        by=By.CSS_SELECTOR, value="div.container-time div.text"
+    )
+    return state_label.text in ("trabajando", "acabas de entrar")
+
+
 def extract_working_hours(driver, date=date.today()):
     day_number = date.day - 1
     week_number = date.isocalendar().week
@@ -61,20 +70,46 @@ def extract_working_hours(driver, date=date.today()):
     return working_hours
 
 
-@click.command()
-@click.argument("mode", type=click.Choice(["in", "out"], case_sensitive=False))
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 @click.option("--dev", is_flag=True, default=False, help="Dev mode")
-def punch(mode, dev):
+def punch(dev):
     with enter_timenet(headless=not dev) as driver:
-        days_working_hours = extract_working_hours(driver)
-        if days_working_hours == 0:
-            return
-        label = "Sortida" if mode == "out" else "Entrada"
+        label = "Sortida" if am_i_working(driver) else "Entrada"
         driver.find_element(
             by=By.CSS_SELECTOR, value=f"button[aria-label={label}]"
         ).click()
         sleep()
 
 
+@cli.command()
+@click.option("--dev", is_flag=True, default=False, help="Dev mode")
+def program(dev):
+    with enter_timenet(headless=not dev) as driver:
+        today = date.today()
+        total_expected_hours = extract_working_hours(driver, date=today)
+        if total_expected_hours == 0:
+            return
+        client = scheduler_v1.CloudSchedulerClient()
+        project_id = "endless-anagram-324913"
+        job_name = "autopunch-scheduler-trigger"
+        location = "us-central1"
+        job = client.get_job(
+            name=f"projects/{project_id}/locations/{location}/jobs/{job_name}"
+        )
+        if total_expected_hours <= 6:
+            times = [9, 9 + total_expected_hours]
+        else:
+            times = [9, 13, 14, 14 + total_expected_hours - 4]
+        cron_hours = ",".join(str(x) for x in times)
+        job.schedule = f"0 {cron_hours} {today.day} {today.month} *"
+        update_mask = field_mask_pb2.FieldMask(paths=["schedule"])
+        client.update_job(job=job, update_mask=update_mask)
+
+
 if __name__ == "__main__":
-    punch()
+    cli()
